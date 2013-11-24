4324dde17ea2286ad22ea9a526ee9a08f6579223
Gabe Conradi <gummybearx@gmail.com>
2013-11-24 11:24:06 -0500

    clean up queue some

--- src/main/scala/com/pipefail/redisqueue/Queue.scala
+++ src/main/scala/com/pipefail/redisqueue/Queue.scala
@@ -3,14 +3,14 @@ package com.pipefail.redisqueue
 import com.redis._
 import com.pipefail.redisqueue._
 
-class Queue(client: RedisClient, namespace: String, queue: String) {
+class Queue(client: RedisClient, queue: String, namespace: String) {
 
   def this(host: String, port: Int, queue: String, namespace: String = "redisqueue") = this(new RedisClient(host, port), queue, namespace)
 
   // set default values for connection
   def this(queue: String, namespace: String = "redisqueue") = this("localhost", 6379, queue, namespace)
 
-  def queueKey = makeKey(None)
+  def inputKey = makeKey(None)
   def failedKey = makeKey(Some("failed"))
   def activeKey = makeKey(Some("active"))
 
@@ -21,31 +21,42 @@ class Queue(client: RedisClient, namespace: String, queue: String) {
 
   def getClient: RedisClient = client
 
-  private def clearQueue(queueKey: String) = client.del(queueKey)
-  def clearInput = clearQueue(queueKey)
+  // TODO: check if sismember, change to boolean response
+  //def removeActive(message: Message) = client.srem(message.toJson, activeKey) match {
+  //  case Some(n) => n
+  //  case _ => 0
+  //}
+
+  private def clearQueue(key: String) = client.del(key)
+  def clearInput = clearQueue(inputKey)
   def clearFailed = clearQueue(failedKey)
 
-  private def length(queueKey: String) = client.llen(queueKey) match {
+  private def cardinality(key: String) = client.scard(key) match {
     case Some(n) => n
     case _ => 0
   }
-  def lengthInput = length(queueKey)
-  def lengthFailed = length(failedKey)
+  private def length(key: String) = client.llen(key) match {
+    case Some(n) => n
+    case _ => 0
+  }
+  def inputLength = length(inputKey)
+  def failedLength= length(failedKey)
+  def activeLength= length(activeKey)
 
   def deleteAll = {
     //TODO: check for consumers before deleting queue
-    List(queueKey, failedKey).map { k => clearQueue(k) }
+    List(inputKey, failedKey).map { k => clearQueue(k) }
   }
 
   //TODO: write function to wrap up talking with client, wraps exceptions
-  def put(message: Message) = client.lpush(queueKey, message.toJson) match {
+  def put(message: Message) = client.lpush(inputKey, message.toJson) match {
     case Some(num) => (true,num)
     case _ => (false,0)
   }
 
-  def requeueFailed: Int = lengthFailed match {
+  def requeueFailed: Int = failedLength match {
     case 0 => 0
-    case _ => client.rpoplpush(failedKey,queueKey) match {
+    case _ => client.rpoplpush(failedKey,inputKey) match {
       case None => 0
       case Some(_) => 1 + requeueFailed
     }
